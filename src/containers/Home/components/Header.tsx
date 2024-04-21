b216ea898ee347c8c7ddd2a6c9c514079a0accbc
yunusmertofficial <yunusmertofficial@gmail.com>
2024-04-21 23:58:01 +0300

    Refactor file paths and imports for authentication components and utils

--- src/containers/Home/components/Header.tsx
+++ src/containers/Home/components/Header.tsx
@@ -4,26 +4,7 @@ import { Icon, Text, Button } from "@rneui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SearchBar } from "../../../components/SearchBar";
-
-const colors = {
-  primary: "#38B2AC", // Turquoise
-  secondary: "#FFD700", // Golden Yellow
-  background: "#F5F5F5", // Light Gray
-  white: "#FFFFFF", // Pure White
-  black: "#000000", // Pure Black
-  grey0: "#FFFFFF", // Pure White
-  grey1: "#F2F2F2", // Very Light Gray
-  grey2: "#E0E0E0", // Light Gray
-  grey3: "#CCCCCC", // Medium Gray
-  grey4: "#999999", // Dark Gray
-  grey5: "#333333", // Dark Gray
-  greyOutline: "#CCCCCC", // Medium Gray
-  searchBg: "#E0E0E0", // Light Gray
-  success: "#4CAF50", // Green
-  error: "#F44336", // Red
-  warning: "#FFEB3B", // Yellow
-  divider: "#E0E0E0", // Light Gray
-};
+import { colors } from "../../../utils";
 
 export const Header = ({
   modalVisible,
